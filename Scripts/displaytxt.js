b120fd0ffe6e2496814b7bfaadb615f3f88142d2
Aleonic <arodriguez8303@gmail.com>
2023-12-16 18:54:46 -0500

    Initial Commit

---
+++ Scripts/displaytxt.js
@@ -0,0 +1,21 @@
+var typed = new Typed("#role", {
+    strings: [
+        "Software Developer",
+        "Web Developer",
+        "Software Tester",
+        "Game Developer",
+        "Programmer",
+        "Creator",
+        "Builder",
+        "Designer",
+        "3D Modeler",
+        "Computer Scientist",
+        "Database Architect",
+        "Database Administrator"
+        ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 5000,
+    loop: true,
+    smartBackspace: false
+})
---
+++ Scripts/updateYear.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+
+const yearDisplay = document.getElementById("ccYear");
+
+yearDisplay.innerHTML = year;
